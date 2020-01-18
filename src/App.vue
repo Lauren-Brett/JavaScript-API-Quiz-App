60b3ab01f5d01a2a5960e4bbb49562020fb14849
lauren brett <laurencareybrett@gmail.com>
2020-01-18 23:44:47 +0000

    get data to page

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
 
   </div>
-  <!-- loop over using key, index look at object.key -->
+  <!--  look at object.key -->
 </template>
 
 <script>
@@ -39,10 +39,6 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
